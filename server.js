cf0d85c1095fc9765dc710e5a1aaf435ebd4abce
Igor Korman <ikorman12@gmail.com>
2022-11-01 20:54:20 -0400

    added server code, imported packages, modularized files

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+// Import and require mysql2
+const mysql = require('mysql2');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // MySQL username,
+    user: 'root',
+    // TODO: Add MySQL password
+    password: 'Nala1027',
+    database: 'books_db'
+  },
+  console.log(`Connected to the books_db database.`)
+);
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
